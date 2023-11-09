0d0b7c6814209711780987a5bd8ec19313aaca64
Ismael <ismaelguedessilva@gmail.com>
2023-11-09 08:10:23 -0300

    first commit

---
+++ 01.py
@@ -0,0 +1,14 @@
+numint = 1
+numfloat = 2.1
+numstr = "String"
+numbol = True
+numlist = [0, 1, 2, 3, 4]
+numtuple = (100, 94.05, 62, 65)
+numdic =  {'Ismael': 1.65, 'Ana': 1.60, 'João': 1.70}
+
+data = (input("Digite seu nome: "))
+
+print("a tipagem python é dividida em:", "\n Inteiro: ", numint ,"\n Float: ", numfloat, "\n String: ", numstr, "\n Booleano: ", numbol
+      ,"\n Lista(array): ", numlist, "\n Tupla: ", numtuple, "\n Dicionário: ", numdic)
+
+print("O Dado digitado pelo usuário foi: ", data)
